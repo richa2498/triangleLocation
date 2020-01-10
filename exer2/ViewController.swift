0374fc09c317ad0cced4fcf76dedb20efc05a6a6
richa <richa1998.patel@gmail.com>
2020-01-10 18:07:50 -0500

    Initial Commit

---
+++ exer2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  exer2
+//
+//  Created by MacStudent on 2020-01-10.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
